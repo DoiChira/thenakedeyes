15fd52d811153dc6e15fc5beb416eda6e49e90e4
Tinkal Deka <tinkald@policyadvisor.com>
2023-12-21 10:55:29 +0530

    Add MUI theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './components/shared/Navbar';
 import Routes from './components/Routes';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme/index';
 
 function App() {
   return (
-    <div className=''>
+    <ThemeProvider theme={theme}>
       <Navbar />
       <Routes />
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/index.jsx
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4338ca',
+      dark: "#312e81",
+      light: "#6366f1"
+    },
+  },
+});
+
+export default theme;
